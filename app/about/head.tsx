028067c1a382c241cd2c5d0e8022cc39746138a2
adie <hello@adie.codes>
2022-12-07 06:03:05 +0000

    Add about page

---
+++ app/about/head.tsx
@@ -0,0 +1,8 @@
+export default function AboutHead() {
+  return (
+    <>
+      <title>About Adie</title>
+      <meta name="description" content="Adie's following the tutorial" />
+    </>
+  );
+}

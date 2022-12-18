ee33a29731ddbc663c493749999b923c8d45e372
adie <hello@adie.codes>
2022-12-18 16:50:12 +0000

    Handle loading

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -2,5 +2,8 @@ export default async function HelloPage() {
   const req = await fetch('http://localhost:3000/api/hello');
   const data = await req.json();
 
+  // proc the loading state
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   return <p>{data.name}</p>;
 }
